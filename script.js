b1e4ee64cfaa3eb6c8a918d7355849ec34e8f920
Katoma610 <55027295+Katoma610@users.noreply.github.com>
2022-04-23 18:16:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ let menuImg5 = document.querySelector('.menuItem5');
 
 menuImg1.addEventListener('click', function(){
     let varVideo = document.querySelector('#videoSlider');
-    varVideo.src = '/video/cloud.mp4';
+    varVideo.src = 'video/cloud.mp4';
 });
 
 menuImg2.addEventListener('click', function(){
     let varVideo = document.querySelector('#videoSlider');
-    varVideo.src = '/video/islands.mp4';
+    varVideo.src = 'video/islands.mp4';
 });
 
 menuImg3.addEventListener('click', function(){
     let varVideo = document.querySelector('#videoSlider');
-    varVideo.src = '/video/moon.mp4';
+    varVideo.src = 'video/moon.mp4';
 });
 
 menuImg4.addEventListener('click', function(){
     let varVideo = document.querySelector('#videoSlider');
-    varVideo.src = '/video/star.mp4';
+    varVideo.src = 'video/star.mp4';
 });
 
 menuImg5.addEventListener('click', function(){
     let varVideo = document.querySelector('#videoSlider');
-    varVideo.src = '/video/wood.mp4';
+    varVideo.src = 'video/wood.mp4';
 });
 
 let timer;
@@ -39,11 +39,11 @@ function changeTitle(icon, text){
 
 window.onblur = () => {
     timer = setTimeout(() => {
-        changeTitle('/video/pause.png', '😭😭Вернись пожалуйста😭😭')
+        changeTitle('video/pause.png', '😭😭Вернись пожалуйста😭😭')
     }, 3000);
 }
 
 window.onfocus = () => {
     clearTimeout(timer);
-    changeTitle('/video/play.png', titleOld);
+    changeTitle('video/play.png', titleOld);
 }
